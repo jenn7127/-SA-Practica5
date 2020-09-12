415d962a57fcad9bc3c92a434d8c88c344177d9a
Jennifer <jenny7127@gmail.com>
2020-09-11 18:23:34 -0600

    first commit

---
+++ ESB/ESB/ESB.svc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ESB
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ESB : IESB
+    {
+        public string CambiarEstado(int NoOrden)
+        {
+            var client = new WebServlast.Service1();
+            return client.CambiarEstado(NoOrden, true);
+        }
+
+        public string CrearOrden()
+        {
+
+            var client = new WebServRestaurante.WSRestaurante();
+            return client.CrearOrden();
+            
+        }
+
+        public string EnviarOrden(int NoOrden)
+        {
+            var client = new WebSRepartidor.WSRepartidor();
+            return client.EnviarOrden(NoOrden, true);
+        }
+
+        public Orden ObtenerEstado(int NoOrden)
+        {
+
+            
+            var client = new WebServRestaurante.WSRestaurante();
+            string estado1 = client.ObtenerEstado(NoOrden, true).estado;
+            string error = client.ObtenerEstado(NoOrden, true).Error;
+            if (estado1 == null)
+            {
+                return new Orden() { Error = error };
+            }
+            else
+            {
+                return new Orden() { estado = estado1 };
+            }
+
+
+        }
+
+        public Orden ObtenerEstadoCli(int NoOrden)
+        {
+            var client = new WebServlast.Service1();
+            string estado1 = client.ObtenerEstadoCli(NoOrden, true).estado;
+            string error = client.ObtenerEstadoCli(NoOrden, true).Error;
+            if (estado1 == null)
+            {
+                return new Orden() { Error = error };
+            }
+            else
+            {
+                return new Orden() { estado = estado1 };
+            }
+
+        }
+
+        public Orden ObtenerEstadoRep(int NoOrden)
+        {
+            var client = new WebSRepartidor.WSRepartidor();
+            string estado1 = client.ObtenerEstadoRep(NoOrden, true).estado;
+            string error = client.ObtenerEstadoRep(NoOrden, true).Error;
+            if (estado1 == null)
+            {
+                return new Orden() { Error = error };
+            }
+            else
+            {
+                return new Orden() { estado = estado1 };
+            }
+
+        }
+    }
+}
---
+++ WebService01/Cliente/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEstadoRes_Click(object sender, EventArgs e)
+        {
+            int orden  = short.Parse(txtOrdenRes.Text);
+            using (WSEsb.ESBClient client = new WSEsb.ESBClient())
+            {
+                var EstadoOrden = client.ObtenerEstado(orden);
+                string estado= EstadoOrden.estado;
+                string error = EstadoOrden.Error;
+                if (estado != null)
+                {
+                    lblOrdenRes.Text = estado;
+                    Console.WriteLine("Estado obtenido del webservice del restaurante: " + estado);
+                }
+                else
+                {
+                    lblOrdenRes.Text = error;
+                    Console.WriteLine("Estado obtenido del webservice del restaurante: " + error);
+                }
+            }
+
+        }
+
+        private void btnNewOrden_Click(object sender, EventArgs e)
+        {
+            using(WSEsb.ESBClient client = new WSEsb.ESBClient())
+            {
+                lblCrearOrden.Text = client.CrearOrden();
+            }
+        }
+
+        private void btnOrdenRep_Click(object sender, EventArgs e)
+        {
+            int orden = short.Parse(txtOrdenRep.Text);
+            using (WSEsb.ESBClient client = new WSEsb.ESBClient())
+            {
+                var EstadoOrden = client.ObtenerEstadoRep(orden);
+                string estado = EstadoOrden.estado;
+                string error = EstadoOrden.Error;
+                if (estado != null)
+                {
+                    lblOrdenRep.Text = estado;
+                    Console.WriteLine("Estado obtenido del webservice del repartidor: " + estado);
+                }
+                else
+                {
+                    lblOrdenRep.Text = error;
+                    Console.WriteLine("Estado obtenido del webservice del repartidor: " + error);
+                }
+            }
+        }
+    }
+}
